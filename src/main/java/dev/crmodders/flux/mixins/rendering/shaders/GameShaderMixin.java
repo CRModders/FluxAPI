449c5d45573361fcbe4332b75b2c99d112c963dd
repletsin5 <50048371+repletsin5@users.noreply.github.com>
2024-03-30 19:55:28 +0000

    Use Identifier instead of my old ASSET_KEY in GameShaderMixin

--- src/main/java/dev/crmodders/flux/mixins/rendering/shaders/GameShaderMixin.java
+++ src/main/java/dev/crmodders/flux/mixins/rendering/shaders/GameShaderMixin.java
@@ -1,26 +1,22 @@
 package dev.crmodders.flux.mixins.rendering.shaders;
 
 import com.badlogic.gdx.files.FileHandle;
-import dev.crmodders.flux.FluxConstants;
+import dev.crmodders.flux.tags.Identifier;
 import finalforeach.cosmicreach.GameAssetLoader;
 import finalforeach.cosmicreach.rendering.shaders.GameShader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.util.regex.Pattern;
-
 @Mixin(GameShader.class)
 public class GameShaderMixin {
     @Redirect(method = "loadShaderFile", at = @At(value = "INVOKE", target = "Lfinalforeach/cosmicreach/GameAssetLoader;loadAsset(Ljava/lang/String;)Lcom/badlogic/gdx/files/FileHandle;"))
     FileHandle loadShaderFromMods(String fileName) {
-        String noFolder = fileName.replace("shaders/","");
-        if (noFolder.contains(FluxConstants.ASSET_KEY)) {
-            String[] split = noFolder.split(Pattern.quote(FluxConstants.ASSET_KEY));
-            if(split.length!= 2) {
-                return null;
-            }
-            return GameAssetLoader.loadAsset(split[0] + FluxConstants.ASSET_KEY + "shader/" + split[1]);
+        String noFolder = fileName.replace("shaders/", "");
+        if (noFolder.contains(":")) {
+            Identifier id = Identifier.fromString(noFolder);
+            id.name = "shaders/" + id.name;
+            return GameAssetLoader.loadAsset(id.toString());
         }
         return GameAssetLoader.loadAsset(fileName);
     }
