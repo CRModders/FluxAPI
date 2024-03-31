112236423c095a75c87e8f761b924efedc08ce83
Mr-Zombii <thezombiigames@gmail.com>
2024-03-31 01:26:34 -0500

    Add IFunctionalBlock for backwards compatibility

---
+++ src/main/java/dev/crmodders/flux/api/block/IFunctionalBlock.java
@@ -0,0 +1,14 @@
+package dev.crmodders.flux.api.block;
+
+import finalforeach.cosmicreach.blocks.BlockPosition;
+import finalforeach.cosmicreach.blocks.BlockState;
+import finalforeach.cosmicreach.entities.Player;
+import finalforeach.cosmicreach.world.Zone;
+
+public interface IFunctionalBlock extends IModBlock {
+
+    default void onInteract(Zone zone, Player player, BlockState blockState, BlockPosition position) {}
+    default void onPlace(Zone zone, Player player, BlockState blockState, BlockPosition position) {}
+    default void onBreak(Zone zone, Player player, BlockState blockState, BlockPosition position) {}
+
+}
