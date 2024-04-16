1ff619a7bee9c19a368639600ec343bc00680517
nanobass <marschue09@gmail.com>
2024-04-17 00:53:08 +0200

    added logback (broken at the moment)

--- src/main/java/dev/crmodders/flux/FluxAPI.java
+++ src/main/java/dev/crmodders/flux/FluxAPI.java
@@ -1,38 +1,29 @@
 package dev.crmodders.flux;
 
-import com.badlogic.gdx.graphics.Color;
 import dev.crmodders.flux.api.block.impl.TestBlock;
 import dev.crmodders.flux.api.events.GameEvents;
 import dev.crmodders.flux.api.factories.FactoryFinalizer;
-import dev.crmodders.flux.api.generators.BlockModelGenerator;
 import dev.crmodders.flux.localization.LanguageFile;
 import dev.crmodders.flux.localization.TranslationApi;
 import dev.crmodders.flux.registry.FluxRegistries;
-import dev.crmodders.flux.tags.Identifier;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FluxAPI implements ModInitializer {
 
-    static Logger logger = LoggerFactory.getLogger("FluxAPI");
+    public static final Logger LOGGER = LoggerFactory.getLogger("FluxAPI");
 
     @Override
     public void onInitialize() {
-        logger.info("Flux Fabric Initialized");
+        LOGGER.info("Flux Fabric Initialized");
+
+        FluxRegistries.BLOCK_FACTORIES.add(new FactoryFinalizer<>(TestBlock::new));
 
         GameEvents.ON_REGISTER_LANGUAGE.register(() -> {
             LanguageFile lang = LanguageFile.loadLanguageFile(FluxConstants.LanguageEnUs.load());
             TranslationApi.registerLanguageFile(lang);
         });
-
-        FluxRegistries.BLOCK_FACTORIES.add(new FactoryFinalizer<>(() -> {
-            TestBlock block = new TestBlock();
-            for (BlockModelGenerator generator : block.getBlockModelGenerators(Identifier.fromString("fluxapi:test_block"))) {
-                generator.createColoredCuboid(0, 0, 0, 69, 69, 69, "test_model", Color.BLUE);
-            }
-            return block;
-        }));
 
     }
 }
