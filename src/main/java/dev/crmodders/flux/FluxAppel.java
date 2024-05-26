8d5ad66d21f51213935c2618408f05b69f492298
Mr-Zombii <thezombiigames@gmail.com>
2024-05-25 23:21:21 -0500

    Updated FluxAppel. Changed the name of the FluxAPI2 to ExampleEntrypointUser.

--- src/main/java/dev/crmodders/flux/FluxAppel.java
+++ src/main/java/dev/crmodders/flux/FluxAppel.java
@@ -2,19 +2,21 @@ package dev.crmodders.flux;
 
 import dev.crmodders.flux.events.OnRegisterLanguageEvent;
 import dev.crmodders.flux.logging.LoggingAgent;
-import dev.crmodders.flux.logging.api.MicroLogger;
+import dev.crmodders.flux.logging.impl.CustomizableLogger;
+import dev.crmodders.flux.logging.impl.SimpleColouredLogger;
 import net.appel.mod.interfaces.ModInitializer;
 import org.greenrobot.eventbus.Subscribe;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class FluxAppel implements ModInitializer {
 
-    public static final MicroLogger LOGGER = LoggingAgent.getLogger("FluxAPI");
+    public static final CustomizableLogger LOGGER = LoggingAgent.getLogger("FluxAPIAppel", CustomizableLogger.class);
 
     @Override
     public void onInit() {
-        LOGGER.info("Flux Initialized");
+        LOGGER.setMessageColorizer((level, string) -> string);
+        LoggingAgent.setDefaultLoggingClass(SimpleColouredLogger.class);
+
+        LOGGER.info("Flux Appel Edition Initialized");
         FluxRegistries.EVENT_BUS.register(this);
     }
 
